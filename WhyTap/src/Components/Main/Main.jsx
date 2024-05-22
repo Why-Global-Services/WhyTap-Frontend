d70c407b95c468684c56a42036e7679a03b671c8
BobertGomez <bobert@whyglobalservices.com>
2024-05-22 15:21:29 +0530

    component push

---
+++ WhyTap/src/Components/Main/Main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+function Main() {
+  return (
+    <div>
+        
+        <Outlet />
+    </div>
+  )
+}
+
+export default Main
