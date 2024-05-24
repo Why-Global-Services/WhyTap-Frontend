61139c6a5afe95330530fd22584bb4ebed9dc4b5
BobertGomez <bobert@whyglobalservices.com>
2024-05-24 09:19:10 +0530

    banner

--- WhyTap/src/Components/Home/HomePage.jsx
+++ WhyTap/src/Components/Home/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Banner from './HomeComponents/Banner'
+import SecondSection from './HomeComponents/SecondSection'
 
 function HomePage() {
   return (
-    <div className='w-full pt-10'>
+    <div className='w-full'>
+      <div className='w-full h-6'></div>
       <Banner />
+      <SecondSection />
       </div>
   )
 }
